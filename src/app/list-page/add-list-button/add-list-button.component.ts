5bce35732aab2d0e11b98f5e5688fc8a3d4da345
AdrielHercules <109773902+AdrielHercules@users.noreply.github.com>
2025-06-30 20:10:12 +0200

    First commit

---
+++ src/app/list-page/add-list-button/add-list-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-list-button',
+  imports: [],
+  templateUrl: './add-list-button.component.html',
+  styleUrl: './add-list-button.component.css'
+})
+export class AddListButtonComponent {
+
+}
