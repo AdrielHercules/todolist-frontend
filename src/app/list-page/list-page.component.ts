bd2214ae75657e26f74d95f4b5d76afff34e1eb2
AdrielHercules <109773902+AdrielHercules@users.noreply.github.com>
2025-07-01 18:45:54 +0200

    Add add-list-button component into list-page

--- src/app/list-page/list-page.component.ts
+++ src/app/list-page/list-page.component.ts
@@ -3,10 +3,11 @@ import { List } from '../models/list';
 import { ListItemComponent } from './list-item/list-item.component';
 import { RouterLink } from '@angular/router';
 import { ListService } from '../services/list.service';
+import { AddListButtonComponent } from './add-list-button/add-list-button.component';
 
 @Component({
   selector: 'app-list-page',
-  imports: [ListItemComponent, RouterLink],
+  imports: [ListItemComponent, RouterLink, AddListButtonComponent],
   templateUrl: './list-page.component.html',
   styleUrl: './list-page.component.css',
 })
@@ -16,5 +17,9 @@ export class ListPageComponent {
 
   constructor() {
     this.lists = this.listService.getLists();
+  }
+
+  onAddListClick() {
+    console.log('Not implemented.');
   }
 }
