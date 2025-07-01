1344a5bd1a2c474f4e9e05c706d1308f192b7c27
AdrielHercules <109773902+AdrielHercules@users.noreply.github.com>
2025-07-01 18:01:22 +0200

    Refactor app to include updated top-bar component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopBarComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'todolist-frontend';
