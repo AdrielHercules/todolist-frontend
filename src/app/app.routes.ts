c2f7aa84783ad0a157a97ac6732fcdb8f2b30962
AdrielHercules <109773902+AdrielHercules@users.noreply.github.com>
2025-07-01 16:19:57 +0200

    Add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { ListPageComponent } from './list-page/list-page.component';
+import { TaskPageComponent } from './task-page/task-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ListPageComponent,
+    title: 'Listas',
+  },
+  {
+    path: 'tasks',
+    component: TaskPageComponent,
+    title: 'Lista de tareas',
+  },
+];
