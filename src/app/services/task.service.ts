d9c30e7e67079c5a5ac01e9cd864dcf6d6208fc1
NestorLT <158834348+NestorLT@users.noreply.github.com>
2025-07-01 17:33:06 +0200

    add some taks in the new variable taskList and the method getTask()

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Task } from '../models/task';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
+  taskList: Task[] = [
+    {
+      id: '1',
+      icon: 'icon-list',
+      text: 'First task',
+      completed: false,
+    },
+    {
+      id: '2',
+      icon: 'icon-list',
+      text: 'Second task',
+      completed: false,
+    },
+    {
+      id: '3',
+      icon: 'icon-list',
+      text: 'Third task',
+      completed: false,
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
+  getTasks() {
+    return this.taskList;
+  }
 }
