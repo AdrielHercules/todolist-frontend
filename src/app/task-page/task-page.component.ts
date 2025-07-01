53d0734acbdb0304eaf5a7a3e280776d569cd0cb
NestorLT <158834348+NestorLT@users.noreply.github.com>
2025-07-01 17:33:33 +0200

    Refactor task page and item components to display tasks with icons and checkboxes

--- src/app/task-page/task-page.component.ts
+++ src/app/task-page/task-page.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TaskService } from '../services/task.service';
+import { Task } from '../models/task';
+import { TaskItemComponent } from './task-item/task-item.component';
+
 
 @Component({
   selector: 'app-task-page',
-  imports: [],
+  imports: [TaskItemComponent],
   templateUrl: './task-page.component.html',
   styleUrl: './task-page.component.css'
 })
 export class TaskPageComponent {
 
+  taskService: TaskService = inject(TaskService);
+  listTaks: Task[] = [];
+
+  constructor () {
+    this.listTaks = this.taskService.getTasks();
+  }
 }
