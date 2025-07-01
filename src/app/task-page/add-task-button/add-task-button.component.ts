aac651ff8fc0d0904e576d7af7b4c45ca4a3a178
NestorLT <158834348+NestorLT@users.noreply.github.com>
2025-07-01 16:18:21 +0200

    Created task components and task-page

---
+++ src/app/task-page/add-task-button/add-task-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-task-button',
+  imports: [],
+  templateUrl: './add-task-button.component.html',
+  styleUrl: './add-task-button.component.css'
+})
+export class AddTaskButtonComponent {
+
+}
