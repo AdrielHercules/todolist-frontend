a503479c12e7280d26b1aeb46637740355f57cb1
AdrielHercules <109773902+AdrielHercules@users.noreply.github.com>
2025-07-12 19:28:51 +0200

    Remove unused imports in list-page component

--- src/app/list-page/list-page.component.ts
+++ src/app/list-page/list-page.component.ts
@@ -1,13 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { List } from '../models/list';
 import { ListItemComponent } from './list-item/list-item.component';
-import { RouterLink } from '@angular/router';
 import { ListService } from '../services/list.service';
 import { AddListButtonComponent } from './add-list-button/add-list-button.component';
 
 @Component({
   selector: 'app-list-page',
-  imports: [ListItemComponent, RouterLink, AddListButtonComponent],
+  imports: [ListItemComponent, AddListButtonComponent],
   templateUrl: './list-page.component.html',
   styleUrl: './list-page.component.css',
 })
