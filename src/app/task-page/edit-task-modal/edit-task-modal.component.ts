b386ab1c84e4637c57adbb7453c7c596c980e836
NestorLT <158834348+NestorLT@users.noreply.github.com>
2025-07-16 22:39:48 +0200

    Add EditTaskModal component

---
+++ src/app/task-page/edit-task-modal/edit-task-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+import { Task } from '../../models/task';
+
+@Component({
+  selector: 'app-edit-task-modal',
+  imports: [],
+  templateUrl: './edit-task-modal.component.html',
+  styleUrl: './edit-task-modal.component.css',
+})
+export class EditTaskModalComponent {}
