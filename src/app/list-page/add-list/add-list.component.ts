93b7e6d66527e552914a518591e8c7342f64c03a
AdrielHercules <109773902+AdrielHercules@users.noreply.github.com>
2025-07-18 12:59:05 +0200

    Implement add-list modal for creating new lists

--- src/app/list-page/add-list/add-list.component.ts
+++ src/app/list-page/add-list/add-list.component.ts
@@ -1,11 +1,156 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, viewChild } from '@angular/core';
+import { List } from '../../models/list';
+import { FormsModule } from '@angular/forms';
+import { ModalComponent } from '../../core/modals/modal/modal.component';
 
 @Component({
   selector: 'app-add-list',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-list.component.html',
-  styleUrl: './add-list.component.css'
+  styleUrl: './add-list.component.css',
 })
-export class AddListComponent {
+export class AddListComponent extends ModalComponent<Partial<List>> implements AfterViewInit {
+  protected name = '';
+  protected icon = '📋';
+  protected showIcons = false;
 
+  private inputName = viewChild<ElementRef<HTMLInputElement>>('inputName');
+
+  protected icons: string[] = [
+    '😀',
+    '📋',
+    '🕧',
+    '💣',
+    '🛒',
+    '🍐',
+    '⭐',
+    '🍈',
+    '🍎',
+    '🎄',
+    '😊',
+    '🅰️',
+    '🏛️',
+    '😃',
+    '😄',
+    '😁',
+    '😆',
+    '😅',
+    '🤣',
+    '😂',
+    '🙂',
+    '🙃',
+    '🫠',
+    '😉',
+    '🧼',
+    '💼',
+    '💊',
+    '🎮',
+    '🍌',
+    '🍇',
+    '🍉',
+    '🍓',
+    '🥝',
+    '🍍',
+    '🥥',
+    '🥑',
+    '🌽',
+    '🥕',
+    '🏠',
+    '🏡',
+    '🏢',
+    '🏬',
+    '🏫',
+    '🏥',
+    '🏦',
+    '🧠',
+    '🫀',
+    '🫁',
+    '🦴',
+    '🦷',
+    '📅',
+    '📆',
+    '🕒',
+    '⏰',
+    '🗓️',
+    '✏️',
+    '🖊️',
+    '🖋️',
+    '📝',
+    '📖',
+    '🔒',
+    '🔓',
+    '🔑',
+    '🗝️',
+    '❤️',
+    '💛',
+    '💚',
+    '💙',
+    '💜',
+    '🖤',
+    '🤍',
+    '🤎',
+    '🚀',
+    '✈️',
+    '🚗',
+    '🚲',
+    '🛴',
+    '🛵',
+    '🎉',
+    '🎊',
+    '🎁',
+    '🎈',
+    '🎂',
+    '🍰',
+    '⚽',
+    '🏀',
+    '🏈',
+    '⚾',
+    '🎾',
+    '🏐',
+    '🏉',
+  ];
+
+  protected isClosing: boolean;
+
+  constructor() {
+    super();
+    this.isClosing = false;
+  }
+
+  override ngAfterViewInit() {
+    this.inputName()?.nativeElement.focus();
+  }
+
+  onCreateClick() {
+    if (this.name === '' || this.icon == '') {
+      return;
+    }
+
+    this.confirmed.emit({
+      name: this.name,
+      icon: this.icon,
+    });
+
+    this.close();
+  }
+
+  onInputClick() {
+    if (this.showIcons) this.showIcons = false;
+  }
+
+  close() {
+    this.isClosing = true;
+  }
+
+  onAnimationEnd(): void {
+    if (this.isClosing) {
+      this.closed.emit();
+      this.isClosing = false;
+    }
+  }
+
+  onIconClicked(icon: string) {
+    this.icon = icon;
+    this.showIcons = false;
+  }
 }
--- src/app/list-page/list-page.component.ts
+++ src/app/list-page/list-page.component.ts
@@ -3,6 +3,8 @@ import { List } from '../models/list';
 import { ListItemComponent } from './list-item/list-item.component';
 import { ListService } from '../services/list.service';
 import { AddListButtonComponent } from './add-list-button/add-list-button.component';
+import { AddListComponent } from './add-list/add-list.component';
+import { ModalService } from '../core/modals/modal.service';
 
 @Component({
   selector: 'app-list-page',
@@ -12,6 +14,8 @@ import { AddListButtonComponent } from './add-list-button/add-list-button.compon
 })
 export class ListPageComponent {
   private listService: ListService = inject(ListService);
+  private modalService = inject(ModalService);
+
   lists: List[] = [];
 
   constructor() {
@@ -19,6 +23,10 @@ export class ListPageComponent {
   }
 
   onAddListClick() {
-    console.log('Not implemented.');
+    const component = this.modalService.openModal<Partial<List>>(AddListComponent);
+
+    component?.confirmed.subscribe((list) => {
+      this.listService.addList(list);
+    });
   }
 }
