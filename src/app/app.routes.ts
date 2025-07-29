b7201c80a1e184f0aeb92f1dedf19478c3ba27d8
AdrielHercules <109773902+AdrielHercules@users.noreply.github.com>
2025-07-29 19:37:52 +0200

    Add user-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ListPageComponent } from './list-page/list-page.component';
 import { TaskPageComponent } from './task-page/task-page.component';
+import { UserPageComponent } from './user-page/user-page.component';
 
 export const routes: Routes = [
   {
@@ -12,5 +13,10 @@ export const routes: Routes = [
     path: 'tasks/:listId',
     component: TaskPageComponent,
     title: 'Lista de tareas',
+  },
+  {
+    path: 'user',
+    component: UserPageComponent,
+    title: 'Usuario',
   },
 ];
---
+++ src/app/user-page/user-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { IconButtonComponent } from '../shared/components/icon-button/icon-button.component';
+
+@Component({
+  selector: 'app-user-page',
+  imports: [IconButtonComponent],
+  templateUrl: './user-page.component.html',
+  styleUrl: './user-page.component.css',
+})
+export class UserPageComponent {}
