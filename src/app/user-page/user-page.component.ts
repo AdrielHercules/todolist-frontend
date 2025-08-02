33927241d4e45fbe01e88f5f9d2d7b6c1f26a709
AdrielHercules <109773902+AdrielHercules@users.noreply.github.com>
2025-08-02 20:14:44 +0200

    Add TopBar configuration in user-page

--- src/app/user-page/user-page.component.ts
+++ src/app/user-page/user-page.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { IconButtonComponent } from '../shared/components/icon-button/icon-button.component';
+import { TopBarService } from '../layout/top-bar/services/top-bar.service';
+import { TopBarButtonType } from '../layout/top-bar/models/top-bar-button-type';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-page',
@@ -7,4 +10,18 @@ import { IconButtonComponent } from '../shared/components/icon-button/icon-butto
   templateUrl: './user-page.component.html',
   styleUrl: './user-page.component.css',
 })
-export class UserPageComponent {}
+export class UserPageComponent {
+  topbarService = inject(TopBarService);
+  router = inject(Router);
+
+  constructor() {
+    this.topbarService.setConfig({
+      transparent: true,
+      leftButtons: [{ type: TopBarButtonType.BACK, callback: this.goBack.bind(this) }],
+    });
+  }
+
+  goBack() {
+    this.router.navigate(['/']);
+  }
+}
