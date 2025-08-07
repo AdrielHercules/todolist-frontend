dc54a06fc0370390a61278fe5471aea8e0fb0bea
AdrielHercules <109773902+AdrielHercules@users.noreply.github.com>
2025-08-07 14:58:29 +0200

    Refactor add-button to use Icons.ts

--- src/app/shared/components/add-button/add-button.component.ts
+++ src/app/shared/components/add-button/add-button.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { Icons } from '../../icons';
 
 @Component({
   selector: 'app-add-button',
   imports: [],
   templateUrl: './add-button.component.html',
-  styleUrl: './add-button.component.css'
+  styleUrl: './add-button.component.css',
 })
 export class AddButtonComponent {
-
+  protected icons = Icons;
 }
