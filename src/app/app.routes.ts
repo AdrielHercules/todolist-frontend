ab00c2299e6039195bb387d6d20a7d0030990eed
NestorLT <158834348+NestorLT@users.noreply.github.com>
2025-08-07 11:22:46 +0200

    Add login and signup page routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { ListPageComponent } from './list-page/list-page.component';
 import { TaskPageComponent } from './task-page/task-page.component';
 import { UserPageComponent } from './user-page/user-page.component';
+import { LoginPageComponent } from './login-page/login-page/login-page.component';
+import { SignupPageComponent } from './signup-page/signup-page/signup-page.component';
 
 export const routes: Routes = [
   {
@@ -18,5 +20,15 @@ export const routes: Routes = [
     path: 'user',
     component: UserPageComponent,
     title: 'Usuario',
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent,
+    title: 'Login page',
+  },
+  {
+    path: 'signup',
+    component: SignupPageComponent,
+    title: 'Signup page',
   },
 ];
