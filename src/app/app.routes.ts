4099cc7c410f112dcb7abefcd0b4bbcc05db30d7
NestorLT <158834348+NestorLT@users.noreply.github.com>
2025-08-16 20:35:51 +0200

    Move signup page files from signup-page/signup-page to signup-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { ListPageComponent } from './list-page/list-page.component';
 import { TaskPageComponent } from './task-page/task-page.component';
 import { UserPageComponent } from './user-page/user-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
-import { SignupPageComponent } from './signup-page/signup-page/signup-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
 
 export const routes: Routes = [
   {
