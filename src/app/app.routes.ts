dd6d218b25e3ad29a0807359d8face34934c5192
NestorLT <158834348+NestorLT@users.noreply.github.com>
2025-08-16 20:28:53 +0200

    Move login page docs from login-page/login-page to login-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { ListPageComponent } from './list-page/list-page.component';
 import { TaskPageComponent } from './task-page/task-page.component';
 import { UserPageComponent } from './user-page/user-page.component';
-import { LoginPageComponent } from './login-page/login-page/login-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 import { SignupPageComponent } from './signup-page/signup-page/signup-page.component';
 
 export const routes: Routes = [
