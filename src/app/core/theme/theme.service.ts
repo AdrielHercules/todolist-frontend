c18a76c16b60d50258729658bec986452645cf9d
AdrielHercules <109773902+AdrielHercules@users.noreply.github.com>
2025-08-30 15:57:36 +0200

    Fix ThemeService not removing previous theme

--- src/app/core/theme/theme.service.ts
+++ src/app/core/theme/theme.service.ts
@@ -6,12 +6,26 @@ import { Themes } from './themes';
 })
 export class ThemeService {
   private root: HTMLElement;
+  private currentTheme = '';
 
   constructor() {
     this.root = document.documentElement;
   }
 
   setTheme(theme: Themes) {
-    this.root.classList.add(`theme-${theme.toString()}`);
+    this.removeCurrentTheme();
+
+    const name = theme.toString();
+    console.debug('Changing theme to: ' + name);
+    this.root.classList.add(`theme-${name}`);
+    this.currentTheme = name;
+  }
+
+  removeCurrentTheme() {
+    if (this.currentTheme == '') return;
+
+    console.debug('Removing theme: ' + this.currentTheme);
+    this.root.classList.remove(`theme-${this.currentTheme}`);
+    this.currentTheme = '';
   }
 }
