4cd8a79b1956fe222e8bbef03224df507790bba5
AdrielHercules <109773902+AdrielHercules@users.noreply.github.com>
2025-09-01 18:33:05 +0200

    Add capacitor support and build android project

---
+++ capacitor.config.ts
@@ -0,0 +1,18 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+import 'dotenv/config';
+
+const config: CapacitorConfig = {
+  appId: 'com.todolist.app',
+  appName: 'Todolist',
+  webDir: 'dist/todolist-frontend/browser',
+  android: {
+    buildOptions: {
+      keystorePath: process.env.KEYSTORE_PATH ?? '',
+      keystorePassword: process.env.KEYSTORE_PASSWORD ?? '',
+      keystoreAlias: process.env.KEYSTORE_ALIAS ?? '',
+      keystoreAliasPassword: process.env.KEYSTORE_ALIAS_PASSWORD ?? '',
+    },
+  },
+};
+
+export default config;
