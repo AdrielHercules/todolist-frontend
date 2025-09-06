5cd3da1c47d0d156550bb62a143ece45880331f5
NestorLT <158834348+NestorLT@users.noreply.github.com>
2025-09-06 21:16:59 +0200

    Initialize database in app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,29 @@
-import { Component, inject } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ModalHostComponent } from './core/modals/modal-host/modal-host.component';
 import { TopBarComponent } from './layout/top-bar/top-bar.component';
 import { ThemeService } from './core/theme/theme.service';
 import { Themes } from './core/theme/themes';
+import { SQLiteService } from './core/database/sqlite.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, TopBarComponent, ModalHostComponent],
   templateUrl: './app.html',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class App {
+export class App implements OnInit {
   protected title = 'todolist-frontend';
-  themeService = inject(ThemeService);
+
+  private themeService = inject(ThemeService);
+  private sqliteService = inject(SQLiteService);
 
   constructor() {
     this.themeService.setTheme(Themes.LIGHT);
+  }
+
+  async ngOnInit() {
+    // TODO: Handle 'phrase' properly
+    await this.sqliteService.initializeDatabase('todolistDB', 'phrase');
   }
 }
